e47236021f7c4e93ed71b5e751450856afedc84a
aztaro97 <aztaro97@gmail.com>
2021-06-24 03:44:10 +0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
-// import './components/css/style.css'
-import Header from "./components/Header";
-import Service from "./components/Service";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Players from "./components/Players"
 
-// import "./components/css/donate.css"
-// import "./components/css/theme.css"
-// import "./components/css/"
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <About />
-      <Service />
-      <Players />
-      <Contact />
-      <Footer />
+  <h1>Hello word</h1>
     </div>
   );
 }
