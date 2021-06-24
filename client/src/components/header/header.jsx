5263a7b78c8a27a3206c0b82589f570e60f01e63
aztaro97 <aztaro97@gmail.com>
2021-06-24 23:52:59 +0400

    Some modification

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
-import { Image, Row, Col , Container, Navbar, Nav} from "react-bootstrap";
-import {Link} from "react-router-dom"
+import { Image, Row, Col, Container, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./header.css";
 
 const TopNavbar = () => {
@@ -9,21 +9,23 @@ const TopNavbar = () => {
     <Container className="">
       <Row>
         <Col lg={4}>
-        <div className="topnav__left">
-              <FaPhoneAlt className="mx-3" />
-              <div className="phoneNumber">
-                <a href="tel:#">+225 27 22 49 86 05</a>
-                <a href="tel:#">+225 05 64 34 59 82</a>
-              </div>
+          <div className="topnav__left">
+            <FaPhoneAlt className="mx-3" />
+            <div className="phoneNumber">
+              <a href="tel:#">+225 27 22 49 86 05</a>
+              <a href="tel:#">+225 05 64 34 59 82</a>
             </div>
+          </div>
         </Col>
         <Col lg={4}>
           <div className="topnav__center">
+            <Link to="/">
             <Image
               width="300"
               src="http://www.sc3yaokouass.universe.wf/wp-content/uploads/2021/05/LOGO-POLYFINANCE-2048x211.png"
               fluid
             />
+            </Link>
           </div>
         </Col>
         <Col lg={4}>
@@ -31,9 +33,7 @@ const TopNavbar = () => {
             <FaMapMarkerAlt className="mx-3" />
             <div className="adress">
               <p>Cocody Angré: Boulevard Latrille, Villa N°964</p>
-              <p>
-                carrefour Adama Sanogo à 150 m
-              </p>
+              <p>carrefour Adama Sanogo à 150 m</p>
               <p>Accès facile: bus 81, 82, 90, 205, 719</p>
             </div>
           </div>
@@ -43,10 +43,15 @@ const TopNavbar = () => {
   );
 };
 
-
 const NavBarBottom = () => {
-    return (
-        <Navbar bg="white" variant="white" style={{display:"flex", justifyContent:"center"}}>
+  return (
+    <Navbar
+      bg="white"
+      variant="white"
+      style={{ display: "flex", justifyContent: "center" }}
+    >
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="navbar__mobile">
         <Nav className="navigation">
           <Link className="nav__link nav-link" to="/">
             Home
@@ -64,23 +69,19 @@ const NavBarBottom = () => {
             Contact
           </Link>
         </Nav>
-      </Navbar>
-
-    )
-
-}
-
-
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 function Header() {
   return (
     <div className="pt-4">
-        <Container>
-      <TopNavbar />
-      <hr />
-      <NavBarBottom />
-      
-    </Container>
+      <Container>
+        <TopNavbar />
+        <hr />
+        <NavBarBottom />
+      </Container>
     </div>
   );
 }
